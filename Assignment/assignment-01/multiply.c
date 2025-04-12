03a4820c8cd44f7b1706b5b086c4253e38ae625e
sabilar-rahman <sabilarrahman36@gmail.com>
2025-04-12 21:35:50 +0600

    assignment-01

---
+++ Assignment/assignment-01/multiply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    long long int A, B, C;
+    scanf("%lld %lld", &A, &B);
+    C = A * B;
+    printf("%d", C);
+    return 0;
+}
