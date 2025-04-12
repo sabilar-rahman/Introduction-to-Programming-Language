03c57794735e589e71d1cac3626756702a170824
sabilar-rahman <sabilarrahman36@gmail.com>
2025-04-12 21:19:15 +0600

    loop

---
+++ Module 3/hi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// int main()
+// {
+//     for (int j = 5; j <= 10; j++)
+//     {
+//         printf("hi \n");
+//         j++;
+//     }
+// };
+#include <stdio.h>
+
+// int main()
+// {
+//     int i = -5;
+//     while(i < 3)
+//     {
+//         printf("hello ");
+//         i += 2;
+//     }
+//     return 0;
+// }
+
+int main()
+{
+    int i = 5;
+    while (i < 10)
+    {
+        printf("%d i \n" , i);
+        i++;
+    }
+}
