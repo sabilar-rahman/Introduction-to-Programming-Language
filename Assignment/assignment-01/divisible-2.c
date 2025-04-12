19a61d69ece340a5a7fa112378977a1788c8a762
sabilar-rahman <sabilarrahman36@gmail.com>
2025-04-12 22:15:00 +0600

    divisible 2

---
+++ Assignment/assignment-01/divisible-2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 21; i <= N; i = i+21)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
+
+// another method
+// int N;
+// scanf("%d", &N);
+
+// for(int i = 21; i < N; i++){
+//     if (i % 3 == 0 && i % 7 == 0){
+//         printf("%d\n", i);
+//     }
+// }
+
+// return 0;
