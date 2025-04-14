3887ad21f859777966271d0937e46b06646c731f
sabilar-rahman <sabilarrahman36@gmail.com>
2025-04-15 01:26:45 +0600

    quiz

---
+++ Module 6/quiz.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("hi \n");
+        if (i > 2)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
